af7f364d4f2bb4f4894dd16443c71b67a5b4f0f2
yadavmm30 <54914878+yadavmm30@users.noreply.github.com>
2020-12-07 23:48:42 -0500

    Add files via upload

---
+++ Code/data_download.py
@@ -0,0 +1,19 @@
+import os
+os.system("pip3 install googledrivedownloader")
+os.system("pip3 install requests")
+from google_drive_downloader import GoogleDriveDownloader as gdd
+
+# to download Images
+gdd.download_file_from_google_drive(file_id='1g4ExCFgRTOA6BcO1BMHSfh7u4QpNHM5t',
+                                            dest_path='./images.zip',
+                                            unzip=True)
+
+# to download glove
+gdd.download_file_from_google_drive(file_id='1OMm6HSwSvn-PrYNRGmX6ajQA-mWc-wOr',
+                                            dest_path='./glove.zip',
+                                            unzip=True)
+
+# to download captions
+gdd.download_file_from_google_drive(file_id='1K_c-R2MIBGX9cR7A5N-KozhmoeJ0hBbl',
+                                            dest_path='./glove.zip',
+                                            unzip=True)
